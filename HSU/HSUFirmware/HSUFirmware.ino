7dd2b0c8fe5652c7a8a3d7f9788a9f1f8ea0012f
QuantumPhysGuy <mattwise1985@gmail.com>
2013-02-05 21:03:26 -0500

    Firmware Update
    Updated the firmware to support the MLX90614.

--- HSU/HSUFirmware/HSUFirmware.ino
+++ HSU/HSUFirmware/HSUFirmware.ino
@@ -1,4 +1,5 @@
 #include <EEPROM.h>
+#include <i2cmaster.h>
 #include <SoftwareSerial.h>
 
 SoftwareSerial btConnection = SoftwareSerial(2, 3);
@@ -37,6 +38,9 @@ void setup()
     btConnection.begin(38400);
     btConnection.println("RDY BT ON");
   }
+  
+  i2c_init(); //Initialise the i2c bus for temp readings
+  PORTC = (1 << PORTC4) | (1 << PORTC5);//enable pullups
   
   setRGBColor(0, 255, 0);
 }
@@ -80,15 +84,19 @@ void loop()
 
 void getSensorData()
 {
-  //THIS IS ALL SIMULATED
   setRGBColor(0, 0, 255);
   delay(1500);
+  char buffer[10];
+  //THIS IS SIMULATED
   int iBPM = random(72, 85);
+  //THIS IS SIMULATED
   int SpO2 = random(90, 100);
-  int Temp = random(98, 101);
+  String Temp;
   String sReturn;
   
-  sReturn = "ACK," + (String)iBPM + "," + (String)SpO2 + "," + (String)Temp;
+  Temp = dtostrf(getTemp(false), 5, 2, buffer);
+  
+  sReturn = "ACK," + (String)iBPM + "," + (String)SpO2 + "," + Temp;
   
   if (bUseLocalSerial)
   {
@@ -100,6 +108,45 @@ void getSensorData()
   }
   
   setRGBColor(0, 255, 0);
+}
+
+float getTemp(boolean bReturnCelcius)
+{
+    int dev = 0x5A<<1;
+    int data_low = 0;
+    int data_high = 0;
+    int pec = 0;
+    
+    i2c_start_wait(dev+I2C_WRITE);
+    i2c_write(0x07);
+    
+    // read
+    i2c_rep_start(dev+I2C_READ);
+    data_low = i2c_readAck(); //Read 1 byte and then send ack
+    data_high = i2c_readAck(); //Read 1 byte and then send ack
+    pec = i2c_readNak();
+    i2c_stop();
+    
+    //This converts high and low bytes together and processes temperature, MSB is a error bit and is ignored for temps
+    double tempFactor = 0.02; // 0.02 degrees per LSB (measurement resolution of the MLX90614)
+    double tempData = 0x0000; // zero out the data
+    int frac; // data past the decimal point
+    
+    // This masks off the error bit of the high byte, then moves it left 8 bits and adds the low byte.
+    tempData = (double)(((data_high & 0x007F) << 8) + data_low);
+    tempData = (tempData * tempFactor)-0.01;
+    
+    float celcius = tempData - 273.15;
+    float fahrenheit = (celcius*1.8) + 32;
+    
+    if (bReturnCelcius)
+    {
+      return celcius;
+    }
+    else
+    {
+      return fahrenheit;
+    }
 }
 
 void setRGBColor(int iRed, int iGreen, int iBlue)
